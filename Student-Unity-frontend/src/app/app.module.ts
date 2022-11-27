32992c090d77e2c0951a6eb242d1d2a69b258c16
AhmedElemary57 <honor5766@gmail.com>
2022-11-27 21:52:23 +0200

    accounts page 1 only front need to fix HTML and CSS

--- Student-Unity-frontend/src/app/app.module.ts
+++ Student-Unity-frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountComponent
+    AccountComponent,
+    AccountPageComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
