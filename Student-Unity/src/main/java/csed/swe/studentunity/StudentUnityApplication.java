950fe264f746f78531c19acbb526902ac3bc2f5c
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2022-11-27 14:29:40 +0200

    first commit

---
+++ Student-Unity/src/main/java/csed/swe/studentunity/StudentUnityApplication.java
@@ -0,0 +1,13 @@
+package csed.swe.studentunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentUnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentUnityApplication.class, args);
+    }
+
+}
---
+++ Student-Unity/src/test/java/csed/swe/studentunity/StudentUnityApplicationTests.java
@@ -0,0 +1,13 @@
+package csed.swe.studentunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentUnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
