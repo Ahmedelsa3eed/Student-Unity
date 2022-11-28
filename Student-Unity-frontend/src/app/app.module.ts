d3118fbbf9d8a95040ae054bc2d31c237ed9bccf
Mohamed Khaled <es-mohamed.khaled.abbas20@alexu.edu.eg>
2022-11-29 00:47:19 +0200

    changign app-component and index title, removing default angular html, adding empty sign-in component, sign-in-out service, and user model

--- Student-Unity-frontend/src/app/app.module.ts
+++ Student-Unity-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingInComponent } from './components/sing-in/sing-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent
   ],
   imports: [
     BrowserModule,
