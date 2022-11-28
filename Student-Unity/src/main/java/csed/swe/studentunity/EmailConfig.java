11b1dd2c47bf737b2242289ee2e00ea65ea41fef
AmrElkhatieb <amr1kemon1@gmail.com>
2022-11-28 22:29:08 +0200

    Sign in

---
+++ Student-Unity/src/main/java/csed/swe/studentunity/EmailConfig.java
@@ -0,0 +1,34 @@
+package csed.swe.studentunity;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("amr1kemon1@gmail.com");
+        mailSender.setPassword("iviynsragbypisgp");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        return mailSender;
+    }
+}
---
+++ Student-Unity/src/main/java/csed/swe/studentunity/logic/EmailService.java
@@ -0,0 +1,28 @@
+package csed.swe.studentunity.logic;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public EmailService(){}
+
+
+
+
+    public void send(String body, String subject, String toEmail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("amr1kemon1@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+        mailSender.send(message);
+    }
+}
