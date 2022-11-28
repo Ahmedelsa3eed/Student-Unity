2a45e4db3a3c934a34779a73751d888210e107f4
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2022-11-28 23:24:51 +0200

    fix: change account card layout

--- Student-Unity-frontend/src/app/account-page/account-page.component.ts
+++ Student-Unity-frontend/src/app/account-page/account-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
-import {User} from "../models/User";
+import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "../models/User";
 import { AccountsService } from '../services/accounts-service.service';
 
 @Component({
@@ -34,65 +34,9 @@ export class AccountPageComponent implements OnInit {
       }
     });
     // @ts-ignore
-    // this.users$.next([
-    //   {
-    //     "studentId": 1,
-    //     "name": "Leanne Graham",
-    //     "email": "abc@cde.fg",
-    //     "password":"123456",
-    //     "role": "admin",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 2,
-    //     "name": "Ervin Howell",
-    //     "email": "aaaa@gmail.com",
-    //     "password":"123456",
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 3,
-    //     "name": "Clementine Bauch",
-    //     "email": "aaqwq@aadsasd.aada",
-    //     "password":"123456",
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 4,
-    //     "name": "Patricia Lebsack",
-    //     "email": "adsads@gmail.com",
-    //     "password":"11",
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 5,
-    //     "name": "Chelsey Dietrich",
-    //     "email": "adsads@ddd.dd",
-    //     "password":"sdd",
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 6,
-    //     "name": "Mrs. Dennis Schulist",
-    //     "email": "sasas.eee@dsdsd.com",
-    //     "password":"qqqq" ,
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   },
-    //   {
-    //     "studentId": 7,
-    //     "name": "Kurtis Weissnat",
-    //     "email": "abc@gmail.com",
-    //     "password":"sadads",
-    //     "role": "user",
-    //     "sessionID": "1234567890"
-    //   }]);
-
+    // this.users$.next(environment.dummyUsers);
   }
+
   public search() {
     this.searchLoading = true;
     this.accountsService.searchAccounts(this.user, this.searchString).subscribe(res => {
@@ -100,11 +44,9 @@ export class AccountPageComponent implements OnInit {
         this.users$.next(res.body);
         console.log(res.body)
         this.isLoading = false;
-
       }
       this.searchLoading = false;
     });
-
   }
 
 }
