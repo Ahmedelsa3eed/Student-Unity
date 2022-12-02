8bbf2877ac5b988c7c7a6115096e28ea60ae77de
el-sharif-mohamed22 <95565086+el-sharif-mohamed22@users.noreply.github.com>
2022-12-02 18:28:17 +0200

    On branch Sign-up-fron-end Your branch is up to date with 'origin/Sign-up-fron-end'.
    Changes to be committed:
    	modified:   Student-Unity-frontend/src/app/components/sing-up/sing-up.component.html
    	modified:   Student-Unity-frontend/src/app/components/sing-up/sing-up.component.ts
    
    Form Validation was added to the sign up form.

--- Student-Unity-frontend/src/app/components/sing-up/sing-up.component.ts
+++ Student-Unity-frontend/src/app/components/sing-up/sing-up.component.ts
@@ -1,48 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-sing-up',
   templateUrl: './sing-up.component.html',
   styleUrls: ['./sing-up.component.css']
 })
 export class SingUpComponent implements OnInit {
-  constructor() { }
+  registerForm! : FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  
   ngOnInit(): void {
+    this.registerForm = this.fb.group ({
+      firstName: this.fb.control(null, [Validators.required, Validators.maxLength(16), Validators.pattern("[a-zA-Z]*")]),
+      lastName: this.fb.control(null, [Validators.required, Validators.maxLength(16), Validators.pattern("[a-zA-Z]*")]),
+      email: this.fb.control(null, [Validators.required, Validators.pattern("^[A-Za-z0-9._%+-]+@alexu\.edu\.eg")]),
+      password: this.fb.control(null, [Validators.required, Validators.maxLength(16), Validators.minLength(8)]),
+      rPassword: this.fb.control(null, [Validators.required, this.ConfirmedValidator('password', 'rPassword')]),
+      studentId: this.fb.control(null, [Validators.required])
+    }, {
+      validators: this.ConfirmedValidator('password', 'rPassword')
+    });
   }
+
   // Method to register a new user
   registerSubmitted() {
     console.log(this.registerForm.value);
   }
-  registerForm = new FormGroup({
-    firstName: new FormControl("", [Validators.required]),
-    lastName: new FormControl(""),
-    email: new FormControl(""),
-    password: new FormControl(""),
-    rPassword: new FormControl(""),
-    studentId: new FormControl("")
 
-  });
-  get firstName(): FormControl{
+  // creacte a custom validtor to check if the password and the re-entered password are the same
+  private ConfirmedValidator(controlName: string, matchingControlName: string) : ValidatorFn {
+    return (control: AbstractControl) : ValidationErrors | null => {
+      const FormGroup = control as FormGroup;
+      const controlValue  = FormGroup.get(controlName)?.value;
+      const matchingControlValue  = FormGroup.get(matchingControlName)?.value;
+
+      if (control.get('password')?.value === control.get('rPassword')?.value) {
+        return null;
+      } else {
+        return { ValudesNotMatch: true };
+      }
+    }
+  }
+
+
+  // getters for the form controls
+  get FirstName(): FormControl{
     return this.registerForm.get('firstName') as FormControl;
   }
-  get lastName(): FormControl{
+  get LastName(): FormControl{
     return this.registerForm.get('lastName') as FormControl;
   }
-  get email(): FormControl{
+  get Email(): FormControl{
     return this.registerForm.get('email') as FormControl;
   }
-  get password(): FormControl{
+  get Password(): FormControl{
     return this.registerForm.get('password') as FormControl;
   }
-  get rPassword(): FormControl{
+  get RPassword(): FormControl{
     return this.registerForm.get('rPassword') as FormControl;
   }
-  get studentId(): FormControl{
+  get StudentId(): FormControl{
     return this.registerForm.get('studentId') as FormControl;
   }
-  
-
 }
