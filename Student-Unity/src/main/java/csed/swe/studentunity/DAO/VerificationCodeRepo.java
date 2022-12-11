99e7564f4f032bc583924ef7d840df27d619c3e9
mo-ayman <m.ayman.saaed@gmail.com>
2022-12-11 22:59:14 +0200

    Add Verification code table

---
+++ Student-Unity/src/main/java/csed/swe/studentunity/DAO/VerificationCodeRepo.java
@@ -0,0 +1,10 @@
+package csed.swe.studentunity.DAO;
+
+import csed.swe.studentunity.model.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VerificationCodeRepo extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findUserByEmail(String email);
+}
