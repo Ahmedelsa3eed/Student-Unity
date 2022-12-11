8ff025116d76ee0b481a40de2092e917579b5aec
AhmedElemary57 <honor5766@gmail.com>
2022-12-12 01:26:48 +0200

    feedback of the signIn page

--- Student-Unity/src/main/java/csed/swe/studentunity/Logic/LoginService.java
+++ Student-Unity/src/main/java/csed/swe/studentunity/Logic/LoginService.java
@@ -5,8 +5,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 @Transactional
 public class LoginService {
