17a3fd51194abadcadf6fb79e58d2c30d487018a
el-sharif-mohamed22 <el-sharif-mohamed-22@outlook.com>
2022-12-16 07:59:35 +0200

    Changes to be committed: 	modified:   Student-Unity-frontend/src/app/components/all-courses/all-courses.component.css 	modified:   Student-Unity-frontend/src/app/components/all-courses/all-courses.component.html 	modified:   Student-Unity-frontend/src/app/components/all-courses/all-courses.component.ts

--- Student-Unity-frontend/src/app/components/all-courses/all-courses.component.ts
+++ Student-Unity-frontend/src/app/components/all-courses/all-courses.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllCoursesComponent implements OnInit {
 
+  showFilters: boolean = false;
+  checkedItems: string[] = [];
+
   constructor() { }
+
 
   ngOnInit(): void {
   }
+
+  dropDownFilters(): void {
+      this.showFilters = !this.showFilters;
+  }
+
+  itemCheck(event: any): void {
+    let item = event.target;
+    if (!this.checkedItems.includes(item.innerText)) {
+      item.classList.remove('btn-light');
+      item.classList.add('btn-primary');
+      this.checkedItems.push(item.innerText);
+    } else {
+      item.classList.remove('btn-primary');
+      item.classList.add('btn-light');
+      this.checkedItems.splice(this.checkedItems.indexOf(item.innerText), 1);
+    }
+
+    console.log(this.checkedItems);
+  }
+
+  reverse(event: any): void {
+    let expanded = event.target.ariaExpanded;
+    let el = event.target.children[1];
+
+    if (expanded == 'true') {
+      el.classList.remove('down');
+      el.classList.add('up');
+    } else {
+      el.classList.remove('up');
+      el.classList.add('down');
+    }
+
+  }
+
+
 
 }
