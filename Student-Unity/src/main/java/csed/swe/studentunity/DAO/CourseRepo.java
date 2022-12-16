d6c3bec1e2384908aeb7b5e45d1c2bbc87658461
‪Mohamed Khaled‬‏ <104741681+Mkhaled707@users.noreply.github.com>
2022-12-16 16:54:19 +0200

    Implementing some requests regarding registered courses retrieval and manipulation  (#44)
    * adding basic implementation for my courses frontend and backend requirements
    
    * integrating my courses backend with all courses backend
    
    * fixing errors at both frontend and backend
    
    * Adding registered courses enteties and dummy api endpoints
    
    * adding register course, unregister course, and get registered courses requests implementation
    
    * continue resolving conflicts
    
    * made courses service layer to retrun status code instead of http response
    
    * STUD-87- fix tests to make the marge
    
    Co-authored-by: AhmedElemary57 <honor5766@gmail.com>

--- Student-Unity/src/main/java/csed/swe/studentunity/DAO/CourseRepo.java
+++ Student-Unity/src/main/java/csed/swe/studentunity/DAO/CourseRepo.java
@@ -2,6 +2,7 @@ package csed.swe.studentunity.DAO;
 
 import csed.swe.studentunity.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,8 @@ public interface CourseRepo extends JpaRepository<Course, String> {
     void deleteCourseByCode(String code);
 
     Optional<Course> findCourseByCode(String code);
+    @Query(value = "SELECT * FROM course WHERE id = ?1", nativeQuery = true)
+    Optional<Course> findCourseById(long id);
 
     List<Course> findCourseByStatus(boolean status);
 
