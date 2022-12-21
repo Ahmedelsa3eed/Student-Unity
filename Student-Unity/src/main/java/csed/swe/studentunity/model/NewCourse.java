bf92586e7bdfb0c98a5074b4105cbb1616a8005b
AhmedElemary57 <honor5766@gmail.com>
2022-12-21 17:45:09 +0200

    Connection between front and back for filter

---
+++ Student-Unity/src/main/java/csed/swe/studentunity/model/NewCourse.java
@@ -0,0 +1,16 @@
+package csed.swe.studentunity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class NewCourse {
+    String name;
+    String code;
+    Integer term;
+
+
+}
